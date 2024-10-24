92df5c8d10174f860ddff6c3504acc0d34a9ec3b
Nami-59 <145928226+Nami-59@users.noreply.github.com>
2024-10-24 22:12:42 +0300

    Create setup.py

---
+++ .github/workflows/setup/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='tpbp_pr2',
+  version='0.1.0',
+  packages=find_packages(),
+  install_requires=[
+      'requests',
+    #...
+  ],
+)
